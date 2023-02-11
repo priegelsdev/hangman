33cce61f2cb86455c0853c5eb420186c3e757d22
priegelsdev <priegelsdev@gmail.com>
2023-02-11 13:25:26 +0100

    add keyboard with grid styling

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -3,9 +3,8 @@ type HangmanWordProps = {
 };
 
 export default function HangmanWord({ word }: HangmanWordProps) {
-  console.log(word);
   const wordSplit = word.split('');
-  console.log(wordSplit);
+
   const letters = word.split('').map((letter, index) => (
     <span className="letter-blank">
       <span key={index} className="letter">
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,3 +1,42 @@
+// alphabet array for the keyboard component
+
+const ALPHABET_ARRAY = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
 export default function Keyboard() {
-  return <div>Hello</div>;
+  const keys = ALPHABET_ARRAY.map((char, index) => (
+    <button className="key" key={index}>
+      {char}
+    </button>
+  ));
+
+  console.log(keys);
+
+  return <div className="keyboard-container">{keys}</div>;
 }
