00d0e535323a9e755ade9b205d94536c17dd1e02
priegelsdev <priegelsdev@gmail.com>
2023-02-11 10:56:18 +0100

    add letter blanks and letters

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -4,6 +4,15 @@ type HangmanWordProps = {
 
 export default function HangmanWord({ word }: HangmanWordProps) {
   console.log(word);
+  const wordSplit = word.split('');
+  console.log(wordSplit);
+  const letters = word.split('').map((letter, index) => (
+    <span className="letter-blank">
+      <span key={index} className="letter">
+        {letter}
+      </span>
+    </span>
+  ));
 
-  return <div>Hello</div>;
+  return <div className="word">{letters}</div>;
 }
