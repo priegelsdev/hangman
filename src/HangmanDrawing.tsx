db91a26739ffd5e4f6d42b4bba658002a289a388
priegelsdev <priegelsdev@gmail.com>
2023-02-11 10:16:34 +0100

    create hangman drawing style

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,3 +1,21 @@
+const HEAD = <div className="head"></div>;
+const BODY = <div className="body"></div>;
+const LEFT_ARM = <div className="arm-left"></div>;
+const RIGHT_ARM = <div className="arm-right"></div>;
+const LEFT_LEG = <div className="leg-left"></div>;
+const RIGHT_LEG = <div className="leg-right"></div>;
+
+const BODY_PARTS = [HEAD, BODY, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG];
+
 export default function HangmanDrawing() {
-  return <div>Hello</div>;
+  return (
+    <div className="drawing-container">
+      <div className="scaffold-beam-hor"></div>
+      <div className="scaffold-beam-vert"></div>
+      <div className="scaffold-beam-vert small"></div>
+      <div className="scaffold-bottom"></div>
+
+      {BODY_PARTS}
+    </div>
+  );
 }
