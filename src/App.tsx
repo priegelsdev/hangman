68d8224e29834675143f2fa5ce05c715229ef538
priegelsdev <priegelsdev@gmail.com>
2023-02-11 09:24:19 +0100

    initialize vite typescript project, set up eslint and create wordlist.json

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+  return <div>Hello</div>;
+}
+
+console.log('test');
+console.log(ok);
+
+if (console.log(test)) {
+  console.lgo('yes');
+}
